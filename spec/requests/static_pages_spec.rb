63a625e30dcb0a9aeb5acee364a0f6f4a628e06c
hideyamiyazaki <hideyamiyazaki0037@gmail.com>
2017-09-19 18:27:07 +0900

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,34 @@
 require 'spec_helper'
-
 describe "Static pages" do
-  let(:base_title){"Ruby on Rails Tutorial Sample App"}
-  subject {page}
+
+  subject { page }
+
   describe "Home page" do
-    before {visit root_path}
+    before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    before {visit help_path}
+    before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
   end
 
   describe "About page" do
-    before {visit about_path}
+    before { visit about_path }
 
-    it { should have_content('About Us') }
-    it { should have_title(full_title('About Us')) }
+    it { should have_content('About') }
+    it { should have_title('Ruby on Rails Tutorial Sample App | About Us') }
   end
 
   describe "Contact page" do
-    before {visit contact_path}
+    before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it { should have_title('Ruby on Rails Tutorial Sample App | Contact') }
   end
-
 end
